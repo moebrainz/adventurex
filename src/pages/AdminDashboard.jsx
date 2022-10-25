655fa8bf18e18e6eb62373f8898ab78c14640e00
moebrainz <moebrainz@gmail.com>
2022-10-25 10:16:29 +0100

    enable logout

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import DashboardContent from "../components/DashboardContent";
 import Layout from "./Layout";
-// import useLogin from "../components/hooks/useLogin";
-// import { useNavigate } from "react-router-dom";
+import useLogin from "../components/hooks/useLogin";
+import { useNavigate } from "react-router-dom";
 
 const AdminDashboard = () => {
-  // React.useEffect(() => {
-  //   if (!auth.accessToken) {
-  //     navigate("/login");
-  //   }
-  // }, []);
+  React.useEffect(() => {
+    if (!auth.accessToken) {
+      navigate("/login");
+    }
+  }, []);
 
-  // const navigate = useNavigate();
-  // const { auth } = useLogin();
+  const navigate = useNavigate();
+  const { auth } = useLogin();
 
   return (
     <Layout pageTitle="Dashboard">
