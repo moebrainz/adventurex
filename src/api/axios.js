cd1ce6583d3f487a0250c978b7ea762e4c4b435b
ezehuche <ezehuche4@gmail.com>
2022-10-27 11:32:35 +0100

    Updated API endpoint

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,6 +3,6 @@ import axios from "axios";
 // api for login in
 export default (() => {
   return axios.create({
-    baseURL: "https://thexplorex-be.herokuapp.com/api/v1/admin-auth",
+    baseURL: "https://explorerx.herokuapp.com/api/v1/admin-auth",
   });
 })();
